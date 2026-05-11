6a3a6f544d1ee8229ebf723c915e6cce02811dc3
aakashyadav-coder <aakashyadav94486@gmail.com>
2026-05-11 14:03:17 +0500

    deleted

--- android/app/src/main/kotlin/com/example/hamro_pasal/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hamro_pasal
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hamro_pasal/main.dart';
-
-void main() {
-  test('app widget can be constructed', () {
-    expect(const HamroPasalApp(), isA<HamroPasalApp>());
-  });
-}
